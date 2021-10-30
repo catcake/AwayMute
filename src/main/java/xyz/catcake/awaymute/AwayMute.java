0028f53b0294629003dff348120296f8bb8b4e7e
catcake <cat@catcake.xyz>
2021-10-30 03:09:04 -0400

    Implementing AutoMute

--- src/main/java/xyz/catcake/awaymute/AwayMute.java
+++ src/main/java/xyz/catcake/awaymute/AwayMute.java
@@ -1,3 +1,37 @@
 package xyz.catcake.awaymute;
 
-public final class AwayMute {}
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.sound.SoundCategory;
+import org.lwjgl.glfw.GLFW;
+
+public final class AwayMute {
+	private float originalVolume;
+	private boolean wasFocused;
+
+	public AwayMute() {
+		originalVolume = 1;
+		wasFocused = true;
+	}
+
+	public void execute(final GameOptions settings) {
+		final var windowHandle = MinecraftClient.getInstance().getWindow().getHandle();
+		final var isFocused = GLFW.glfwGetWindowAttrib(windowHandle, GLFW.GLFW_FOCUSED) == 1;
+
+		if (wasFocused && !isFocused) mute(settings);
+		else if (!wasFocused && isFocused) unmute(settings);
+	}
+
+	private void mute(final GameOptions settings) {
+		originalVolume = settings.getSoundVolume(SoundCategory.MASTER);
+		settings.setSoundVolume(SoundCategory.MASTER, 0);
+		AwayMuteMod.info("window unfocused; volume muted");
+		wasFocused = false;
+	}
+
+	private void unmute(final GameOptions settings) {
+		settings.setSoundVolume(SoundCategory.MASTER, originalVolume);
+		AwayMuteMod.info(String.format("window refocused; volume restored to %.2f", originalVolume));
+		wasFocused = true;
+	}
+}
---
+++ src/main/java/xyz/catcake/awaymute/mixin/SoundSystemMixin.java
@@ -0,0 +1,23 @@
+package xyz.catcake.awaymute.mixin;
+
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.sound.SoundSystem;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.catcake.awaymute.AwayMuteMod;
+
+@Mixin(SoundSystem.class)
+public abstract class SoundSystemMixin {
+	@Shadow
+	@Final
+	private GameOptions settings;
+
+	@Inject(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At("HEAD"))
+	private void onSoundPlay(CallbackInfo callbackInfo) {
+		AwayMuteMod.getAwayMute().execute(settings);
+	}
+}
