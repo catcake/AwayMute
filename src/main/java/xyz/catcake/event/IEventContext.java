38bee1d89894c605f2a10ef98d3125ba1df64882
catcake <cat@catcake.xyz>
2022-02-07 18:13:11 -0500

    Adding two methods to help with type checking.

--- src/main/java/xyz/catcake/event/IEventContext.java
+++ src/main/java/xyz/catcake/event/IEventContext.java
@@ -1,4 +1,22 @@
 package xyz.catcake.event;
 
 /** {@link IEventContext} is how the {@link EventBus} routes events to their subscribers without mix-ups. */
-public interface IEventContext {}
+public interface IEventContext {
+
+	/**
+	 * Determines if an object is an instance of {@link IEventContext}.
+	 *
+	 * @param o The object to test.
+	 * @return True if {@code o} is an instance of {@link IEventContext}, otherwise False.
+	 */
+	static boolean isEventContext(final Object o) { return o instanceof IEventContext; }
+
+	/**
+	 * Determines if a class derives {@link IEventContext}.
+	 *
+	 * @param c The class to test.
+	 * @return True if {@code c} derives {@link IEventContext}, otherwise False.
+	 */
+	@SuppressWarnings("BooleanMethodIsAlwaysInverted")
+	static boolean isEventContext(final Class<?> c) { return IEventContext.class.isAssignableFrom(c); }
+}
